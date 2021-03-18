5468107c32b76278818e1d13db788eec40c27d99
heinerbehrends <heinerbehrends@hotmail.com>
2021-03-18 15:38:39 +0100

    added clearProb to dynamic parameters

--- pages/api/simple-sim.ts
+++ pages/api/simple-sim.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { simpleSimulation } from '../../simpleSim/simpleSim';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const { maxPop, birthProb } = JSON.parse(req.body);
-  const virusCounts = simpleSimulation({ birthProb, maxPop });
+  const { maxPop, birthProb, clearProb } = JSON.parse(req.body);
+  const virusCounts = simpleSimulation({ birthProb, clearProb, maxPop });
   res.status(200).json({ virusCounts });
 };

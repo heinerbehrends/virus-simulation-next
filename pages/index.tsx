71c6faabfa77251dbb5a1e2630b58fb42f934b53
heinerbehrends <heinerbehrends@hotmail.com>
2021-03-18 15:39:59 +0100

    Added clearProb and refetch to state hooks, added h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,18 @@ import { useEffect, useState } from 'react';
 export default function Home() {
   const [maxPop, setMaxPop] = useState(1000);
   const [birthProb, setBirthProb] = useState(0.1);
+  const [clearProb, setClearProb] = useState(0.05);
+  const [refetchState, triggerRefetch] = useState(Math.random());
   const { data, status, refetch } = useQuery('simpleSim', async () => {
     const virusCounts = await fetch('./api/simple-sim/', {
-      body: JSON.stringify({ maxPop, birthProb }),
+      body: JSON.stringify({ maxPop, birthProb, clearProb }),
       method: 'POST',
     });
     return virusCounts.json();
   });
   useEffect(() => {
     refetch();
-  }, [maxPop, birthProb]);
+  }, [refetchState]);
 
   if (status === 'loading') {
     return <p>...loading</p>;
@@ -28,11 +30,13 @@ export default function Home() {
         <title>Simple Virus Simulation</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1 style={{ textAlign: 'center' }}>Simple Virus Simulation</h1>
       <SimplePlot data={data.virusCounts} />
       <ValueSlider
         name={'maximum population'}
         value={maxPop}
         setValue={setMaxPop}
+        setRefetch={triggerRefetch}
         min={100}
         max={5000}
         step={50}
@@ -41,6 +45,16 @@ export default function Home() {
         name={'Chance to reproduce'}
         value={birthProb}
         setValue={setBirthProb}
+        setRefetch={triggerRefetch}
+        min={0.01}
+        max={0.2}
+        step={0.01}
+      />
+      <ValueSlider
+        name={'Chance to die'}
+        value={clearProb}
+        setValue={setClearProb}
+        setRefetch={triggerRefetch}
         min={0.01}
         max={0.2}
         step={0.01}

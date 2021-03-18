101663a1c2edc49ce98eaf26cdd950bb02dada3f
heinerbehrends <heinerbehrends@hotmail.com>
2021-03-18 15:43:59 +0100

    added prop types

--- components/ValueSlider.tsx
+++ components/ValueSlider.tsx
@@ -1,40 +1,58 @@
-import React, { useState } from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import styled from 'styled-components';
 import Slider from './SliderStyled';
 
-const InputStyled = styled.button`
+const InputStyled = styled.div`
   margin-left: 8px;
-  padding: 0.4rem 1rem;
-  border: 1px solid lightblue;
-  border-radius: 8px;
+  padding: 1rem 1rem;
   font-size: 16px;
   background-color: white;
+  display: grid;
+  grid-template-columns: 175px 1fr 100px;
+  align-items: center;
 `;
-export default function ValueSlider({ name, value, setValue, min, max, step }) {
+
+type ValueSliderProps = {
+  name: string;
+  value: number;
+  setValue: Dispatch<SetStateAction<number>>;
+  setRefetch: Dispatch<SetStateAction<number>>;
+  min: number;
+  max: number;
+  step: number;
+};
+
+export default function ValueSlider({
+  name,
+  value,
+  setValue,
+  setRefetch,
+  min,
+  max,
+  step,
+}: ValueSliderProps) {
   return (
     <>
-      <InputStyled
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <label htmlFor="speed">{name}</label>
+      <InputStyled>
+        <label htmlFor={name}>{name}</label>
         <Slider
           style={{ paddingRight: '1rem' }}
-          name={'speed'}
+          name={name}
           min={min}
           max={max}
           step={step}
-          // onInput={(event) => {
-          //   setValue(event.target.value);
-          // }}
-          onChange={(event) => {
+          onInput={(event) => {
             setValue(event.target.value);
+          }}
+          onMouseUp={() => {
+            setRefetch(Math.random());
+          }}
+          onTouchEnd={() => {
+            setRefetch(Math.random());
           }}
           value={value}
         />
-        {value}
+        {step < 1 ? `${value * 100} %` : value}
       </InputStyled>
     </>
   );

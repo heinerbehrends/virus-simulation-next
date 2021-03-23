50eff560cdc627e37bf736fc65f7f21811c053c2
heinerbehrends <heinerbehrends@hotmail.com>
2021-03-23 16:51:14 +0100

    added types, changes update method

--- patients/patientWithDrugs.ts
+++ patients/patientWithDrugs.ts
@@ -1,6 +1,24 @@
-import { Drug, ResistantVirus } from '../viruses/resistantVirusTypes';
+import { Drug, Virus } from '../viruses/resistantVirusTypes';
 
-export function createPatient(viruses: ResistantVirus[], maxPop, drugs = []) {
+type Drugs = Drug[];
+
+export type Patient = Readonly<{
+  addDrug: (drug: Drug) => Patient;
+  getDrugs: () => Drugs;
+  getResistantCount: (drug: Drug) => number;
+  getPopDensity: (virusArr: any) => number;
+  getVirusCount: () => number;
+  getViruses: () => Viruses;
+  update: () => Patient;
+}>;
+
+export type Viruses = Readonly<Virus>[];
+
+export function createPatient(
+  viruses: Viruses,
+  maxPop: number,
+  drugs: Drugs = []
+): Patient {
   function addDrug(drug: Drug) {
     return createPatient(viruses, maxPop, [...drugs, drug]);
   }
@@ -11,17 +29,17 @@ export function createPatient(viruses: ResistantVirus[], maxPop, drugs = []) {
     return getViruses().filter((virus) => virus.isResistantAgainst(drug))
       .length;
   }
-  function getPopDensity(virusArr) {
+  function getPopDensity(virusArr: Viruses) {
     return virusArr.length / maxPop;
   }
   function getVirusCount() {
     return viruses.length;
   }
-  function getViruses() {
+  function getViruses(): Viruses {
     return viruses;
   }
-  function update() {
-    // filter the viruses that die
+  function update(): Patient {
+    // Keep only the viruses that are still alive
     const survivingViruses = viruses.filter((virus) => virus.doesSurvive());
     // calculate the population density of the surviving viruses
     const popDensity = getPopDensity(survivingViruses);
@@ -29,9 +47,13 @@ export function createPatient(viruses: ResistantVirus[], maxPop, drugs = []) {
     return createPatient(
       // add the viruses that replicate
       survivingViruses.concat(
-        viruses.filter((virus) => virus.doesReproduce(popDensity, drugs))
+        viruses
+          .filter((virus) => virus.doesReproduce(popDensity, drugs))
+          // calling reproduce returns a new virus with potentially mutated resistances
+          .map((virus) => virus.reproduce())
       ),
-      maxPop
+      maxPop,
+      drugs
     );
   }
   return Object.freeze({
